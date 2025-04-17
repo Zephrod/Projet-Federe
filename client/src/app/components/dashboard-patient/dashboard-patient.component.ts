d4261353ebd914050de8dca9c1dd692ca446d91a
paul415-creator <polo.brunetonm@gmail.com>
2025-04-17 10:21:40 +0100

    PULL ENCULE

---
+++ client/src/app/components/dashboard-patient/dashboard-patient.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-patient',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-patient.component.html',
+  styleUrl: './dashboard-patient.component.css'
+})
+export class DashboardPatientComponent {
+
+}
