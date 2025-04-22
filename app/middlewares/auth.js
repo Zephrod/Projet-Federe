ade1455cc8bebaf9c662bb61da571d85e09b288e
zephrod <valentin.le_fessant@outlouk.fr>
2025-04-22 17:16:07 +0200

    rectification auth middleware

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -1,16 +1,22 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-module.exports = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader) return res.status(401);
-
-  const token = authHeader.split(' ')[1];
+module.exports = async (req, res, next) => {
+  const headerValue = req.headers.authorization ?? req.headers.Authorization;
+  if (!headerValue) return res.sendStatus(401);
+  // Bearer jdkelzfhjezlkhfnjzle
+  const [type, token] = headerValue.split(/\s+/);
+  if (type !== "Bearer") return res.sendStatus(401);
   try {
-    const payload = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = payload; // ← infos du token disponibles dans tes routes
+    const payload = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+    req.user = await User.findByPk(payload.id);
+    if (!req.user) return res.sendStatus(401);
+    //if (!req.user.activated) return res.sendStatus(403);
     next();
-  } catch (err) {
-    res.status(403);
+  } catch (e) {
+    return res.sendStatus(401);
   }
 };
